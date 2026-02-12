50ba7d64ded48ffbf71978b5cf2978dd64d4c49f
gilbertmunuotz <officialgilbert24@gmail.com>
2026-02-12 23:47:04 +0300

    #feat: added services page, profile image with skeleton loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Home from './home/page';
+import HomeComponent from './home/page';
+import ImageComponennt from "./image/page";
+import ServiceComponent from "./services/page";
 
 export default function page() {
-  return (
-    <Home />
-  )
+  return <>
+    <HomeComponent />
+    <ImageComponennt />
+    <ServiceComponent />
+  </>
 }
